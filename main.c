bbfbda7cd203c8abe8eed63addd0ec50ed757aa1
lewissskim <117065679+lewissskim@users.noreply.github.com>
2024-01-09 18:04:06 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
  
 int main(){
-    printf("Hello, World!\n");
+    printf("Hello, World3!\n");
     printf("Hello, World1!\n");    
     printf("Hello, World3!\n");
     printf("Hello, World4!\n");
