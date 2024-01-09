7936459e752cefc8ce9ec069d200135c03327946
lewissskim <lewis.kim@lge.com>
2024-01-09 18:02:27 +0900

    master-cmit2

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
  
 int main(){
-    printf("Hello, World!\n");
+    printf("Hello, World12!\n");
     printf("Hello, World1!\n");    
     printf("Hello, World3!\n");
     return 0;
